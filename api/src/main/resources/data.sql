1f53f75c94d165f0f5eb533472f33fdbf0f29e28
Yann Pereira Garcia <2411551235@fema.edu.br>
2024-10-24 16:32:46 -0300

    feat : definir os parametros do h2, e criar os usuários.

---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario (nome_usuario, email_usuario, password_usuario)
+VALUES ('Yann', 'yann_pg@hotmail.com', '123'),
+       ('Bababoe', 'bababoe@fema.edu.br', '255'),
+       ('Bruno', 'BruninReiDela@fema.edu.br', '985');
